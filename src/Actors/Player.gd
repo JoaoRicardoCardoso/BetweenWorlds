e52182e76506fa88ed05c4df42ddd67960f90cf4
BrunoFCM <bruno_micaelo@hotmail.com>
2021-03-02 10:43:10 +0000

    Added wall slide and jump mechanics

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,8 +1,10 @@
 extends Actor
 
+#Base variables
 export var running_speed = 10000
 export var jumping_speed = 10000
 
+#Dash variables
 export var dash_speed = 12000
 export var dash_duration:float = 0.5
 export var dash_slowdown:float = 0.1
@@ -11,10 +13,22 @@ export var dash_cooldown:float = 0.1
 var dash_counter = 0
 var dash_direction = 0
 
+#Stomp variables
 export var stomp_velocity = 600
 
 var stomp_flag = false
 
+#Wall Slide variables
+export var wall_slide_velocity = 800
+export var wall_jump_velocity_y = 800
+export var wall_jump_velocity_x = 300
+
+var wall_collisions_left = 0
+var wall_collisions_right = 0
+var wall_jump_flag = false
+
+####################################################
+#Dash functions
 func calculate_dash_velocity(input_direction, delta):
 	var out = Vector2(0,0)
 	if is_on_floor() and Input.is_action_just_pressed("ui_dash") and dash_counter <= 0:
@@ -30,6 +44,8 @@ func calculate_dash_velocity(input_direction, delta):
 		dash_counter -= delta
 	return out
 	
+##########################################################
+#Stomp functions
 func calculate_stomp(velocity, input_direction):
 	if is_on_floor() and stomp_flag:
 		stomp_flag = false
@@ -40,7 +56,44 @@ func calculate_stomp(velocity, input_direction):
 		stomp_flag = true
 		
 	return velocity
+	
+#########################################################
+#Wall slide/jump functions
+func _on_right_Area2D_body_entered(body):
+	wall_collisions_right += 1
 
+func _on_left_Area2D2_body_entered(body):
+	wall_collisions_left += 1
+
+func _on_right_Area2D_body_exited(body):
+	wall_collisions_right -= 1
+
+func _on_left_Area2D2_body_exited(body):
+	wall_collisions_left -= 1
+
+func calculate_wall_interaction(velocity, input_direction, delta):
+	var out = velocity
+	
+	if is_on_floor():
+		wall_jump_flag = false
+	
+	if is_on_wall() and not is_on_floor() and velocity.y >= 0:
+		out.y = wall_slide_velocity * delta
+		wall_jump_flag = false
+	
+	if Input.is_action_just_pressed("ui_jump") and not is_on_floor() and not wall_jump_flag:
+		if wall_collisions_left > 0 and wall_collisions_right == 0:
+			out.y -= wall_jump_velocity_y
+			out.x += wall_jump_velocity_x
+		
+		if wall_collisions_right > 0 and wall_collisions_left == 0:
+			out.y -= wall_jump_velocity_y
+			out.x -= wall_jump_velocity_x
+	
+	return out
+	
+#########################################################
+#Base physics functions
 func calculate_velocity(velocity, delta):
 	var out = velocity
 	
@@ -59,8 +112,11 @@ func calculate_velocity(velocity, delta):
 		out.y = 0
 		
 	out.y += delta * gravity
+	
 	if is_on_floor() and Input.is_action_just_pressed("ui_jump"):
 		out.y = -jumping_speed
+	
+	out = calculate_wall_interaction(out, input_direction, delta)
 	
 	return out
 
